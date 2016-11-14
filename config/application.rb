88f1aae557983b141ffeb1360e1b3f45533f2aa3
victor prigent <victor.prigent@edu.escpeurope.eu>
2016-11-14 17:29:09 +0100

    Initial commit with devise template from https://github.com/lewagon/rails-templates

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,10 @@ Bundler.require(*Rails.groups)
 
 module Yoloyourte
   class Application < Rails::Application
+    config.generators do |generate|
+      generate.assets false
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
