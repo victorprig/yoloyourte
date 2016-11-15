3d1af7a830287e736c3c656a13b15bcb2acd7d40
Romuald <romualdescande@gmail.com>
2016-11-15 14:26:39 +0100

    created profile contraller and controller new method

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'profiles/show'
+
+  get 'profiles/edit'
+
+  get 'profiles/update'
+
+  get 'profiles/create'
+
+  get 'profiles/new'
+
+  get 'users/sign_out', to: 'pages#home'
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
